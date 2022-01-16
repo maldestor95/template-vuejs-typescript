1ce80831419b4d89b093b8d5599f01101c80b85d
maldestor95 <maldestor95@gmail.com>
2022-01-16 09:38:16 +0100

    chore(test): update mocha and mochasidebar config

--- mocha.env.js
+++ mocha.env.js
@@ -1,3 +1,4 @@
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development";
+process.env.TS_NODE_FILES = "true";
 // eslint-disable-next-line no-undef
 TS_NODE_COMPILER_OPTIONS = '{"module":"commonjs"}';
